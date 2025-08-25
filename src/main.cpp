0b38c479c2b6e8ec34f2238303064ca98d6e0e23
EnzoAvila05 <enzopaa@hotmail.com>
2025-08-25 13:55:40 -0400

    Base do código feita pelo professor na sala

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
-#include <string>
+#include <stdexcept>
 #include <list>
-#include <locale>
+typedef unsigned int uint;
+typedef unsigned int Vertex;
+using namespace std;
+
+class GraphAL {
+private:
+    uint num_vertices;
+    uint num_edges;
+    list<Vertex>  *adj;
+
+public:
+    GraphAL() : num_vertices(0), num_edges(0) {}
+
+    GraphAL(uint num_vertices) : num_vertices(num_vertices), num_edges(0) {
+        adj = new list<Vertex>[num_vertices];
+    }
+
+    void add_edge(Vertex u, Vertex v) {
+        if (u >= num_vertices || v >= num_vertices || u == v) {
+            throw invalid_argument("Argumentos inválidos");
+        }
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        num_edges++;
+    }
+
+    list<Vertex> get_adj(Vertex u) {
+        return adj[u];
+    }
+
+    uint get_num_vertices() {
+        return num_vertices;
+    };
+
+};
+
+void print_graph(GraphAL g) {
+    for (int u = 0; u < g.get_num_vertices(); ++u) {
+        list<Vertex> adj_u = g.get_adj(u);
+        for (const auto& v : adj_u) {
+            cout << v << ", ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
+    GraphAL g(4);
 
-    std::list<std::string> robots = {"Wall-e", "Jarvis", "Blitzcranck"};
+    try {
+        g.add_edge(0, 1);
+        g.add_edge(1, 2);
+        g.add_edge(2, 3);
+        g.add_edge(3, 0);
 
-    std::cout << "Pódio da Corrida de Robôs" << std::endl;
-    int position = 1;
-    for (const auto& robot : robots) {
-        std::cout << position++ << "°: " << robot << std::endl;
+
+        print_graph(g);
+    } catch (const invalid_argument& e) {
+        cout << e.what() << endl;
     }
 
-    auto new_robot = robots.begin();
-    ++new_robot;
-    robots.insert(new_robot, "Robô");
 
-    std::cout << "\nNovo pódio: " << std::endl;
-
-    int position2 = 1;
-    for (const auto& robot : robots) {
-        std::cout << position2++ << "°: " << robot << std::endl;
-    }
-
+    return 0;
 }
